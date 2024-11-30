6c2da187238840491a5d1e4198b99bf6a158cb18
Yana <yana-kim-2002@mail.ru>
2024-11-30 14:17:12 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        peachDark: "#FF642F",
+        peachLight: "#FFD7C9",
+        blueLight: "#E3F1FF",
+        grayLight: "#E8E8E8",
+        grayDark: "#7F7F7F",
+        grayLighter: "#F9F9F9",
+      },
+    },
+    fontFamily: {
+      Montserrat: ["Montserrat", "sans-serif"],
+    },
+    container: {
+      center: true,
+    },
+  },
+  plugins: [],
+};
