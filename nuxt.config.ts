2320f3f487292ddaf8bfdbbc35c0b3ba6c199264
Yana <yana-kim-2002@mail.ru>
2024-12-04 20:34:38 +0300

    gh-pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: "/nuxt-web-recipes/",
+    buildAssetsDir: "assets",
+  },
   compatibilityDate: "2024-11-01",
 
   future: {
